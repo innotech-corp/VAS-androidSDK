8328d67d6d4af89ed148a22444bf431131515dfa
amirhesni <amirhesni@gmail.com>
2018-11-21 11:42:56 +0330

    'url-bug'

--- innovas/src/main/java/com/example/innovas/network/ApiUtils.java
+++ innovas/src/main/java/com/example/innovas/network/ApiUtils.java
@@ -5,7 +5,7 @@ package com.example.innovas.network;
  */
 
 public class ApiUtils {
-    public static final String API_V1 = "api/v1";
+    public static final String API_V1 = "api/v1/";
     public static final String BASE_URL = "http://vas.theinnotech.ir/";
 
 
